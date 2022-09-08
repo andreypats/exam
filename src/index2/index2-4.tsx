0d58638451c9b1321f44cc652ff2769c477f0fc0
andreypats <andrey.it.work@gmail.com>
2022-09-08 11:38:44 +0300

    Exam Tuesday4 is Done, but not all

---
+++ src/index2/index2-4.tsx
@@ -0,0 +1,171 @@
+import React from 'react';
+
+// 1
+
+// type ActionType = {
+//     type: "SUM"|"SUB"|"MULT"|"DIV"
+//     payload: number
+// }
+//
+// export const calculator = (state: number, action: ActionType) => {
+//     switch (ХХХ) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         default:
+//             return state
+//     }
+// }
+
+//Что надо написать вместо ХХХ, чтобы функция calculator работала?
+// action.type   ++++
+
+
+//2
+
+// type ActionType = {
+//     type: "SUM"|"SUB"|"MULT"|"DIV"
+//     payload: number
+// }
+//
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         default:
+//             return state
+//     }
+// }
+
+//Обработка какого action.type не предусмотрена в функции calculator?
+// "MULT"  ++++
+
+
+//3
+
+// type ActionType = {
+//     type: "SUM"|"SUB"|"MULT"|"DIV"
+//     payload: number
+// }
+//
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         case "MULT":
+//             return state * action.payload
+//         default:
+//             return state
+//     }
+// }
+//
+// const result = calculator(10, {XXX, payload: 5})
+// console.log(result)
+//Что надо написать вместо XXX, что бы переменная result содержала значение 5?
+// type: "SUB"  ???????
+
+
+//4
+
+// type ActionType = {
+//     type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
+//     payload: number
+// }
+//
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         case "EXP":
+//             return state ** action.payload
+//         default:
+//             return state
+//     }
+// }
+
+//Что вернёт такой вызов функции: calculator(10, {type: "MULT", payload: 2})?
+// 10   +++++
+
+
+//5
+
+// type ActionType = {
+//     type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
+//     payload: number
+// }
+//
+// export const calculator = (state: number, action: ActionType): number => {
+//     switch (action.type) {
+//         case "SUM":
+//             return state + action.payload
+//         case "SUB":
+//             return state - action.payload
+//         case "DIV":
+//             return state / action.payload
+//         case "MULT":
+//             return state * action.payload
+//         case "EXP":
+//             return state ** action.payload
+//         default:
+//             return state
+//     }
+// }
+// const result = calculator(10, {XXX, payload: 0})
+// if (!(result - 1)){
+//     console.log("IT-INCUBATOR")
+// }
+
+//Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
+// type: "EXP"   +++++
+
+
+//6
+// type UserType = {
+//     id: number
+//     userName: string
+//     email: string
+//     password: string
+// }
+//
+// type ChangeUserPasswordTypeAT = {
+//     type: "CHANGE-USER-PASSWORD"
+//     payload: {
+//         XXX
+//         YYY
+//     }
+// }
+//
+// export const userReducer =
+//     (state: UserType[], action: ChangeUserPasswordTypeAT): UserType[] => {
+//         switch (action.type) {
+//             case "CHANGE-USER-PASSWORD":
+//                 return state.map(u =>
+//                     u.id === action.payload.id
+//                         ? {...u, password: action.payload.newPassword}
+//                         : u)
+//             default:
+//                 return state
+//         }
+//     }
+
+//Какой код должен быть написан вместо XXX и YYY в типе //ChangeUserPasswordTypeAT, что бы редьюсер работал?
+//В ответе напишите через пробел: XXX  YYY
+//
+// id: number newPassword: string    ????
+
+
